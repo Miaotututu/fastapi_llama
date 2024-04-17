fee52a2aaff36d15b59a0c701a8415f04b08f52c
Runzzzz <runzhong.li@yisquare.com.cn>
2024-04-17 16:58:48 +0800

    add prompt info

--- chatWithModel/chatToSql.py
+++ chatWithModel/chatToSql.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 import torch
 from transformers import LlamaTokenizer, LlamaForCausalLM
-from prompt.prompt_template import prompt_template1
+from chatWithModel.prompt.prompt_template import prompt_template1, RESPONSE_FORMAT_SIMPLE
 from database.mysql_op import get_table_info_list
+import json
 
 model_path = "/root/autodl-fs/CodeLlama-13b-CSpider-sql-sft-epoch2"
 tokenizer = LlamaTokenizer.from_pretrained(model_path)
@@ -17,7 +18,8 @@ class QuestionResponse(BaseModel):
 
 def get_sql(user_question, db_name):
     table_info_list = get_table_info_list()
-    prompt = prompt_template1.format(db_name, ",".join(table_info_list), user_question)
+    prompt = prompt_template1.format(db_name, ",".join(table_info_list), user_question,
+                                     json.dumps(RESPONSE_FORMAT_SIMPLE, ensure_ascii=False, indent=4))
     print(prompt)
     model_input = tokenizer(prompt, return_tensors="pt").to("cuda")
     model.eval()
--- chatWithModel/prompt/prompt_template.py
+++ chatWithModel/prompt/prompt_template.py
@@ -6,6 +6,19 @@ Constraint:1.Please understand the user's intention based on the user's question
 3.You can only use the tables provided in the table structure information to generate sql. If you cannot generate sql based on the provided table structure, please say: "The table structure information provided is not enough to generate sql queries." It is prohibited to fabricate information at will.
 4.Please be careful not to mistake the relationship between tables and columns when generating SQL.
 5.Please check the correctness of the SQL and ensure that the query performance is optimized under correct conditions.
-User Question:{2}
-Please think step by step and SQL Query to run
+6.Please choose the best one from the display methods given below for data rendering, and put the type name into the name parameter value that returns the required format. If you cannot find the most suitable one, use 'Table' as the display method. the available data display methods are as follows: response_line_chart:used to display comparative trend analysis data response_pie_chart:suitable for scenarios such as proportion and distribution statistics response_table:suitable for display with many display columns or non-numeric columns response_scatter_plot:Suitable for exploring relationships between variables, detecting outliers, etc. response_bubble_chart:Suitable for relationships between multiple variables, highlighting outliers or special situations, etc.
+response_donut_chart:Suitable for hierarchical structure representation, category proportion display and highlighting key categories, etc.
+response_area_chart:Suitable for visualization of time series data, comparison of multiple groups of data, analysis of data change trends, etc.
+response_heatmap:Suitable for visual analysis of time series data, large-scale data sets, distribution of classified data, etc.
+User Question:
+    {2}
+Please think step by step and respond according to the following JSON format:
+    {3}
+Ensure the response is correct json and can be parsed by Python json.loads.
 """
+
+RESPONSE_FORMAT_SIMPLE = {
+    "thoughts": "thoughts summary to say to user",
+    "sql": "SQL Query to run",
+    "display_type": "Data display method",
+}
