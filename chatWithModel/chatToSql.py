3dc00ea8373b46fdba0f804a070ea4f7e561083c
Runzzzz <runzhong.li@yisquare.com.cn>
2024-04-17 10:21:54 +0800

    add tokenizer/model test

--- chatWithModel/chatToSql.py
+++ chatWithModel/chatToSql.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel
+import torch
+from transformers import LlamaForCausalLM, LlamaTokenizer
+
+model_id = "/home/model_zoo/LLM/llama2/Llama-2-7b-hf/"
+
+tokenizer = LlamaTokenizer.from_pretrained(model_id)
+
+model = LlamaForCausalLM.from_pretrained(model_id, load_in_8bit=True, device_map='auto', torch_dtype=torch.float16)
 
 
 class QuestionResponse(BaseModel):
