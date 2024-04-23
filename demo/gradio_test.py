eb3691eddb0650d5c971d702121744408c3f608b
Runzzzz <runzhong.li@yisquare.com.cn>
2024-04-23 16:32:23 +0800

    修改部分文件 添加gradio_test

--- demo/gradio_test.py
+++ demo/gradio_test.py
@@ -6,11 +6,11 @@ block = gr.Blocks()
 prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
 
 
-def chatgpt_clone(input_question, history):
+def big_model_chat(input_question, history):
     history = history or []
     s = list(sum(history, ()))
     s.append(input_question)
-    data = {"text": prompt,
+    data = {
             "db_name": "dimp",
             "user_question": input_question}
     print(data)
@@ -19,7 +19,7 @@ def chatgpt_clone(input_question, history):
     if response.status_code != 200:
         return "error"
     resp = response.json()
-    output = resp['result']
+    output = json.dumps(resp)
     history.append((input_question, output))
     return history, history
 
@@ -31,7 +31,7 @@ with block:
     message = gr.Textbox(placeholder=prompt)
     state = gr.State()
     submit = gr.Button("SEND")
-    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
+    submit.click(big_model_chat, inputs=[message, state], outputs=[chatbot, state])
 
 gr.close_all()  # 关闭所有正在运行的端口
 block.launch(debug=True, share=True, server_port=6006)
